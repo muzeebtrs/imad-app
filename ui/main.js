efe07f7667fc98100d8ff1399f8c451a32ab6baa
muzeebtrs <36859463+muzeebtrs@users.noreply.github.com>
2018-03-27 23:38:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 var img = document.getElementById('madi');
-img.onClick = function(){
-    img.style.marginLeft = '10px';
+img.onclick = function(){
+    img.style.marginLeft = '100px';
 };
