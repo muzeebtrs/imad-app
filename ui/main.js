be5981b3f66901af8b1983be4b47b3fa560bd806
muzeebtrs <36859463+muzeebtrs@users.noreply.github.com>
2018-03-29 01:50:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,8 @@
-console.log('Loaded!');
-var img = document.getElementById('madi');
-var marginLeft =0;
-function moveRight(){
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft + 'px';
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+    counter= counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
 }
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-};
